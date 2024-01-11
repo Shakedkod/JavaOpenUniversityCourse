bc5557a9ee29215bea7e12696dc3e35bdc8b859b
ShakedKod <82947988+Shakedkod@users.noreply.github.com>
2024-01-11 10:23:24 +0200

    Part 9 - DONE

--- Part9/src/BinarySearch.java
+++ Part9/src/BinarySearch.java
@@ -1,5 +1,11 @@
 public class BinarySearch
 {
+    /**
+     * order of complexity: O(log(n))
+     * @param ar
+     * @param key
+     * @return
+     */
     public static int binarySearch(int[] ar, int key)
     {
         int low = 0, high = ar.length - 1, mid;
